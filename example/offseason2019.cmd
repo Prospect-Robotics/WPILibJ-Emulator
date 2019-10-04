3d060c9adc3642db74857c28dc3c4154c095fc64
David Daney <david.s.daney@gmail.com>
2019-10-03 22:51:03 -0700

    Add example offseason2019.cmd file for OffSeason2019 'bot code.

---
+++ example/offseason2019.cmd
@@ -0,0 +1,30 @@
+# -*- mode: text -*-
+# joystick.config <instance number> <num_axis> <num_buttons> <num_pov>
+joystick.config 0 4 12 1   # <instance number> <num_axis> <num_buttons> <num_pov>
+joystick.config 1 3 14 1
+disable
+# autonomous: 0 -> Not Autonomous, non-zero -> Atonomous
+autonomous 0
+
+# All commands before first wait command are executed before robot
+# constructor runs.
+
+uwait 1000000 # uwait <micro-seconds> until taking next action.
+enable
+
+mwait 2000    # mwait <milli-seconds> until taking next action.
+joystick.axis 1 1 -25.0
+
+mwait 2000
+joystick.axis 1 1 0
+
+mwait 1000
+joystick.button 1 4 press  # Front Pistons button.
+mwait 500
+joystick.button 1 4 release
+
+mwait 2000
+disable
+
+mwait 1000
+powerdown
