83cdd8cc808570a79a8ed470c16ddde74b0afb9a
David Daney <david.s.daney@gmail.com>
2019-10-05 19:10:47 -0700

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module robot_sim {
+    exports com.ctre.phoenix;
+    exports com.ctre.phoenix.motion;
+    exports com.ctre.phoenix.motorcontrol;
+    exports com.ctre.phoenix.motorcontrol.can;
+    exports com.ctre.phoenix.signals;
+    exports com.revrobotics;
+    exports edu.wpi.cscore;
+    exports edu.wpi.first.cameraserver;
+    exports edu.wpi.first.networktables;
+    exports edu.wpi.first.wpilibj;
+    exports edu.wpi.first.wpilibj.drive;
+    exports edu.wpi.first.wpilibj.interfaces;
+    exports edu.wpi.first.wpilibj.shuffleboard;
+    exports edu.wpi.first.wpilibj.smartdashboard;
+    exports edu.wpi.first.wpilibj.util;
+}
